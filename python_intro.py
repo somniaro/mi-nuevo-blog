52c34d6f37f552fa4fcfd3c55aded122472e0b86
Somnia <sonia.rod@gmail.com>
2023-06-08 16:25:18 -0500

    Mi aplicación Django Girls, primer commit

---
+++ python_intro.py
@@ -0,0 +1,11 @@
+def Hola(name=None):
+    if name == 'Sonia':
+        print('Holitas, Sonia!')
+    elif name == 'Carlos':
+        print('cómo vas, Carlos?')
+    else:
+        print('Hola quien seas')
+Hola('Carlos')
+
+for i in range(1,8):
+    print(i)
